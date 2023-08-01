17b665b05208101eb00a87388f5b68a1d630ab52
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-08-01 18:32:21 -0400

    Configuring Postman and checking routes

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const http = require("http");
 const express = require("express");
+const { use } = require("./route/Users/usersRouters");
+const usersRouter = require("./route/Users/usersRouters");
 
 // !Server
 const app = express();
 
+//!middleware
+app.use("/", usersRouter);
+
 const server = http.createServer(app);
 
-// ?start the server here
+// !start the server here
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, console.log(`server is hot on port ${PORT}`));
