6d9ed4039423dbdc559fe0b9e3e99af787147c9b
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-08-09 13:43:12 -0400

    comment CRUD
    comment system mostly done.

--- controllers/comments/commentCtrl.js
+++ controllers/comments/commentCtrl.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Comment = require("../../model/Comment/Comment");
 const Post = require("../../model/Post/Post");
 
-// create a comment
+// *create a comment
 exports.createComment = asyncHandler(async (req, res) => {
   // * get the payload
   const { message, author } = req.body;
@@ -28,6 +28,34 @@ exports.createComment = asyncHandler(async (req, res) => {
   res.json({
     status: "success",
     message: "comment created successfully",
+    comment,
+  });
+});
+
+// * delete a comment
+exports.deleteComment = asyncHandler(async (req, res) => {
+  await Comment.findByIdAndDelete(req.params.commentId);
+  res.status(201).json({
+    status: "success",
+    message: "comment deleted successfully",
+  });
+});
+
+// * update a comment
+exports.updateComment = asyncHandler(async (req, res) => {
+  const comment = await Comment.findByIdAndUpdate(
+    req.params.id,
+    {
+      message: req.body.message,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res.status(201).json({
+    status: "success",
+    message: "comment updated successfully",
     comment,
   });
 });
--- route/comment/commentRouter.js
+++ route/comment/commentRouter.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const isLoggedin = require("../../middlewares/isLoggedin");
-const { createComment } = require("../../controllers/comments/commentCtrl");
+const {
+  createComment,
+  deleteComment,
+  updateComment,
+} = require("../../controllers/comments/commentCtrl");
 
 const commentRouter = express.Router();
 
-// create single category
+//* create a comment
 commentRouter.post("/:postId", isLoggedin, createComment);
+
+//* delete a comment
+commentRouter.delete("/:id", isLoggedin, deleteComment);
+
+//* update a comment
+commentRouter.put("/:id", isLoggedin, updateComment);
 
 module.exports = commentRouter;
